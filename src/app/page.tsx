4cba749e59fd409c5f9f9fc163971f61f54056e3
ashish <a.asis.sigdel01@gmail.com>
2024-05-19 21:20:59 +0545

    Outline bar setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Home } from "@/components/home";
 import { HomeLayout } from "@/components/layout";
+import { Outline } from "@/components/outline";
 import React from "react";
 
 export default function Page() {
   return (
     <div>
       <HomeLayout>
+        <Outline />
         <Home />
       </HomeLayout>
     </div>
---
+++ src/components/outline/index.ts
@@ -0,0 +1,5 @@
+export { default as Outline } from "./Outline";
+export { default as TopBar } from "./TopBar";
+export { default as LeftBar } from "./LeftBar";
+export { default as RightBar } from "./RightBar";
+export { default as BottomBar } from "./BottomBar";
