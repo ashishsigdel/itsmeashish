166e1eee4ec47dc774a4256a5bacc7946caaf361
ashish <a.asis.sigdel01@gmail.com>
2024-05-19 19:58:24 +0545

    Install Scss and setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.scss";
 import { ToastUtlis } from "@/utlis";
 
 const inter = Inter({ subsets: ["latin"] });
