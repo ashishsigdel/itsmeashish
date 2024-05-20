2cfab9dbdd3604c8f929c7a9f930835175b9acdd
ashish <a.asis.sigdel01@gmail.com>
2024-05-20 22:50:51 +0545

    preloader component added

---
+++ src/components/preloader/Preloader.tsx
@@ -0,0 +1,14 @@
+import "@/styles/preloader.scss";
+
+export default function Preloader() {
+  return (
+    <div className="🤚">
+      <div className="👉"></div>
+      <div className="👉"></div>
+      <div className="👉"></div>
+      <div className="👉"></div>
+      <div className="🌴"></div>
+      <div className="👍"></div>
+    </div>
+  );
+}
