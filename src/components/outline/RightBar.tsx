9bbbb09465e1bc19bddb6dd055d0d3bdd3e36143
ashish <a.asis.sigdel01@gmail.com>
2024-05-22 21:18:02 +0545

    Adjust home at center

---
+++ src/components/outline/RightBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { FaFingerprint, FaSkiing } from "react-icons/fa";
+import { FaDesktop } from "react-icons/fa6";
+import { IoHome } from "react-icons/io5";
+import { RiApps2Fill, RiContactsBook3Fill } from "react-icons/ri";
+
+export default function RightBar() {
+  return <div className="hidden md:flex flex-col w-[40px] h-full"></div>;
+}
