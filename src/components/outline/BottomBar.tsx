f31368486430a15b555c48bf0052b36364bbe073
ashish <a.asis.sigdel01@gmail.com>
2024-09-08 22:11:55 +0545

    Add social links

--- src/components/outline/BottomBar.tsx
+++ src/components/outline/BottomBar.tsx
@@ -4,6 +4,8 @@ import { FiList } from "react-icons/fi";
 import { IoMdSettings } from "react-icons/io";
 import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import Link from "next/link"; // Import Link component from Next.js
+import { socialLinks } from "@/types/constant"; // Import your social links
 
 export default function TopBar() {
   return (
@@ -12,36 +14,62 @@ export default function TopBar() {
         <IoMdSettings className="text-[18px] text-white/60 hover:text-primary" />
       </div>
       <div className="pr-[20px] h-full flex justify-center items-center gap-3">
-        <div className="group relative flex flex-col justify-center items-center cursor-pointer hover:text-primary text-white/60">
-          <FaLinkedin />
-          <span className="absolute -top-16 opacity-0 group-hover:-top-8 group-hover:opacity-100 bg-white text-black text-xs rounded py-1 px-2 transition-all duration-300 whitespace-nowrap z-[999]">
-            Linkedin
-          </span>
-        </div>
-        <div className="group relative flex flex-col justify-center items-center cursor-pointer hover:text-primary text-white/60">
-          <FaInstagram />
-          <span className="absolute -top-16 opacity-0 group-hover:-top-8 group-hover:opacity-100 bg-white text-black text-xs rounded py-1 px-2 transition-all duration-300 whitespace-nowrap z-[999]">
-            Instagram
-          </span>
-        </div>
-        <div className="group relative flex flex-col justify-center items-center cursor-pointer hover:text-primary text-white/60">
-          <FaFacebook />
-          <span className="absolute -top-16 opacity-0 group-hover:-top-8 group-hover:opacity-100 bg-white text-black text-xs rounded py-1 px-2 transition-all duration-300 whitespace-nowrap z-[999]">
-            Facebook
-          </span>
-        </div>
-        <div className="group relative flex flex-col justify-center items-center cursor-pointer hover:text-primary text-white/60">
-          <FaGithub />
-          <span className="absolute -top-16 opacity-0 group-hover:-top-8 group-hover:opacity-100 bg-white text-black text-xs rounded py-1 px-2 transition-all duration-300 whitespace-nowrap z-[999]">
-            Github
-          </span>
-        </div>
-        <div className="group relative flex flex-col justify-center items-center cursor-pointer hover:text-primary text-white/60">
-          <FaXTwitter />
-          <span className="absolute -top-16 opacity-0 group-hover:-top-8 group-hover:opacity-100 bg-white text-black text-xs rounded py-1 px-2 transition-all duration-300 whitespace-nowrap z-[999]">
-            X
-          </span>
-        </div>
+        <Link
+          href={socialLinks.linkedin}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="group relative flex flex-col justify-center items-center cursor-pointer hover:text-primary text-white/60">
+            <FaLinkedin />
+            <span className="absolute -top-16 opacity-0 group-hover:-top-8 group-hover:opacity-100 bg-white text-black text-xs rounded py-1 px-2 transition-all duration-300 whitespace-nowrap z-[999]">
+              Linkedin
+            </span>
+          </div>
+        </Link>
+        <Link
+          href={socialLinks.instagram}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="group relative flex flex-col justify-center items-center cursor-pointer hover:text-primary text-white/60">
+            <FaInstagram />
+            <span className="absolute -top-16 opacity-0 group-hover:-top-8 group-hover:opacity-100 bg-white text-black text-xs rounded py-1 px-2 transition-all duration-300 whitespace-nowrap z-[999]">
+              Instagram
+            </span>
+          </div>
+        </Link>
+        <Link
+          href={socialLinks.facebook}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="group relative flex flex-col justify-center items-center cursor-pointer hover:text-primary text-white/60">
+            <FaFacebook />
+            <span className="absolute -top-16 opacity-0 group-hover:-top-8 group-hover:opacity-100 bg-white text-black text-xs rounded py-1 px-2 transition-all duration-300 whitespace-nowrap z-[999]">
+              Facebook
+            </span>
+          </div>
+        </Link>
+        <Link
+          href={socialLinks.github}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="group relative flex flex-col justify-center items-center cursor-pointer hover:text-primary text-white/60">
+            <FaGithub />
+            <span className="absolute -top-16 opacity-0 group-hover:-top-8 group-hover:opacity-100 bg-white text-black text-xs rounded py-1 px-2 transition-all duration-300 whitespace-nowrap z-[999]">
+              Github
+            </span>
+          </div>
+        </Link>
+        <Link href={socialLinks.x} target="_blank" rel="noopener noreferrer">
+          <div className="group relative flex flex-col justify-center items-center cursor-pointer hover:text-primary text-white/60">
+            <FaXTwitter />
+            <span className="absolute -top-16 opacity-0 group-hover:-top-8 group-hover:opacity-100 bg-white text-black text-xs rounded py-1 px-2 transition-all duration-300 whitespace-nowrap z-[999]">
+              X
+            </span>
+          </div>
+        </Link>
       </div>
     </div>
   );
---
+++ src/types/constant.ts
@@ -0,0 +1,17 @@
+// Define the interface
+interface SocialLinks {
+  linkedin: string;
+  instagram: string;
+  github: string;
+  facebook: string;
+  x: string;
+}
+
+// Export the socialLinks object
+export const socialLinks: SocialLinks = {
+  linkedin: "https://www.linkedin.com/in/aasisigdel/",
+  instagram: "https://www.instagram.com/aasissigdel",
+  github: "https://www.github.com/ashishsigdel",
+  facebook: "https://www.facebook.com/ashish.sgdl.9/",
+  x: "https://www.x.com/",
+};
