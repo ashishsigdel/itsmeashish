f2ef7b419aad07acdcd39e053a769e23a6c76532
ashish <a.asis.sigdel01@gmail.com>
2024-10-08 22:19:05 +0545

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,15 @@ import "@/styles/globals.scss";
 import { ToastUtlis } from "@/utlis";
 import PreLoader from "@/utlis/PreLoader";
 import { StoreProvider } from "@/provider";
-
+import "./favicon.ico";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ashish Sigdel",
   description: "Ashish Sigdel",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
